40923f41ceae692f82132f6cd389eeaaae1a4ece
SMARTIED <smartied.79@gmail.com>
2025-09-20 22:57:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mapmydeals_starter/' // Make sure this is the exact name of your repository
+  base: '/mapmydeals_starter/' // Add this line
 })
