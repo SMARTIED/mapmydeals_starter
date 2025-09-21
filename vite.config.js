9c6770f64e6ca1b2a98bd9081ef3a9a6fe0d40f8
SMARTIED <smartied.79@gmail.com>
2025-09-21 22:39:19 +0200

    Fix blank page by changing Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mapmydeals_starter/' // 👈 must match GitHub repo name exactly
+  base: '' // 👈 Change this line
 })
