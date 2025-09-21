e1ad51ced245eadbe0203f675c13f5f80682af51
SMARTIED <smartied.79@gmail.com>
2025-09-21 22:07:08 +0200

    Initial commit of project files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mapmydeals_starter/', // must match repo name exactly
+  base: '/mapmydeals_starter/' // 👈 must match GitHub repo name exactly
 })
