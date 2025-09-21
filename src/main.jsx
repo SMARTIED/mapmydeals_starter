0b25d7fe6c52c3575e47094a6a288415613aa15b
SMARTIED <smartied.79@gmail.com>
2025-09-21 22:47:53 +0200

    Final fix: Correct Vite base and switch to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // To this
+import { HashRouter } from "react-router-dom"; // Use HashRouter
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter> // To this
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
